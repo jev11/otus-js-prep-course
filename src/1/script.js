3f346c46e68876f70dc70bb6d8149717aaf418ce
Barannikov, Evgeny (VodafoneZiggo) <evgeny.barannikov@vodafoneziggo.com>
2021-04-24 15:38:44 +0200

    added test for lesson 1

--- src/1/script.js
+++ src/1/script.js
@@ -1,4 +1,6 @@
+'use strict';
 // 1.В переменных a и b хранятся числа. Написать программу, которая выводит в консоль произведение и сумму этих чисел.
+
 export function sum (n1, n2) {
     return n1 + n2;
 }
@@ -7,21 +9,12 @@ export function prod (n1, n2) {
     return n1*n2;
 }
 
-const a = 1;
-const b = 2;
-console.log(sum(a,b));
-console.log(prod(a,b));
-
-// 2.В двух переменных хранятся строки символов. Написать программу, которая выведет в консоль суммарное количество символов в обоих строках.
+// // // 2.В двух переменных хранятся строки символов. Написать программу, которая выведет в консоль суммарное количество символов в обоих строках.
 export function totalLength(str1, str2) {
-    return (str1 + sr2).length;
+    return (str1 + str2).length;
 }
 
-const string1 = "abc";
-const string2 = "def";
-console.log(totalLength(string1,string2));
-
-// 3.*Написать программу, которая запрашивает у пользователя ввод трёхзначного числа, а потом выводит в консоль сумму цифр введённого числа.
+// // // 3.*Написать программу, которая запрашивает у пользователя ввод трёхзначного числа, а потом выводит в консоль сумму цифр введённого числа.
 export function digitSum(digit) {
     const mod1 = digit%100;
     const n1 = (digit-mod1)/100;
@@ -29,6 +22,3 @@ export function digitSum(digit) {
     const n2 = (mod1-mod2)/10;
     return n1+n2+mod2;
 }
-
-const input = prompt('Enter a 3 digit number');
-console.log(digitSum(input));
---
+++ src/1/script.test.js
@@ -0,0 +1,48 @@
+import {sum, prod, digitSum} from "./script.js";
+
+describe ("sum", () => {
+    it("is a function", () =>{
+        expect(sum).toBeInstanceOf(Function);
+    });
+    [
+        [1, 2, 3],
+        [0, 0, 0],
+        [-5, 5, 0]
+    ].forEach(([a,b, result]) => {
+        it(`returns sum of ${a} and ${b} to be equal ${result}`, () => {
+                expect(sum(a, b)).toBe(result);
+        })
+    })
+});
+
+describe ("prod", () => {
+    it("is a function", () =>{
+        expect(prod).toBeInstanceOf(Function);
+    });
+    [
+        [1, 2, 2],
+        [0, 0, 0],
+        [-5, 5, -25],
+        [3, 5, 15]
+    ].forEach(([a,b, result]) => {
+        it(`returns product of ${a} and ${b} to be equal ${result}`, () => {
+                expect(prod(a, b)).toBe(result);
+        })
+    })
+});
+
+describe ("digitSum", () => {
+    it("is a function", () =>{
+        expect(digitSum).toBeInstanceOf(Function);
+    });
+    [
+        [123, 6],
+        [456, 15],
+        [100, 1],
+        [110, 2]
+    ].forEach(([number, result]) => {
+        it(`returns sum of a natural three digit number ${number} to be equal ${result}`, () => {
+                expect(digitSum(number)).toBe(result);
+        })
+    })
+});
