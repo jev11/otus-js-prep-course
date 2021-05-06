075bb8097f1938ff250d3afb32d8418d56d5dd2a
Barannikov, Evgeny (VodafoneZiggo) <evgeny.barannikov@vodafoneziggo.com>
2021-05-06 23:39:47 +0200

    assignment 9

---
+++ src/9/script.js
@@ -0,0 +1,26 @@
+// 1.Даны длины трёх сторон треугольника. Определить, является ли треугольник прямоугольным. a^2+b^2=c^2
+export function rightTriangle(a, b, c) {
+  return (
+    a ** 2 + b ** 2 === c ** 2 ||
+    b ** 2 + c ** 2 === a ** 2 ||
+    c ** 2 + a ** 2 === b ** 2
+  );
+}
+
+// 2.Пользователь вводит число R. Написать программу, которая выведет в консоль длину окружности и площадь круга с радиусом R.
+export function circleCA(r) {
+  const circumference = Math.round(2 * Math.PI * r * 100) / 100;
+  const area = Math.round(Math.PI * r ** 2 * 100) / 100;
+  return [circumference, area];
+}
+
+// *Пользователь вводит числа a, b и c. Написать программу, выводящую корни квадратного уравнения с коэффициентами a, b и c.
+export function quadraticEquation(a, b, c) {
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    return ["No x"];
+  }
+  const x1 = ((-b + Math.sqrt(discriminant)) / 2) * a;
+  const x2 = ((-b - Math.sqrt(discriminant)) / 2) * a;
+  return [x1, x2];
+}
---
+++ src/9/script.test.js
@@ -0,0 +1,51 @@
+import { rightTriangle, circleCA, quadraticEquation } from "./script";
+
+describe("rightTriangle", () => {
+  it("is a function", () => {
+    expect(rightTriangle).toBeInstanceOf(Function);
+  });
+
+  [
+    [5, 5, 7, false],
+    [10, 23, 11, false],
+    [3, 4, 5, true],
+  ].forEach(([a, b, c, result]) => {
+    it(`determines if triangle with sides ${a}, ${b}, ${c} is a 
+    right triangle ${result}`, () => {
+      expect(rightTriangle(a, b, c)).toBe(result);
+    });
+  });
+});
+
+describe("circleCA", () => {
+  it("is a function", () => {
+    expect(circleCA).toBeInstanceOf(Function);
+  });
+  [
+    [10, 62.83, 314.16],
+    [1, 6.28, 3.14],
+    [15, 94.25, 706.86],
+  ].forEach(([radius, circ, area]) => {
+    it(`calculates circumference ${circ} and area ${area}
+     for circle with radius ${radius}`, () => {
+      expect(circleCA(radius)).toStrictEqual([circ, area]);
+    });
+  });
+});
+
+describe("quadraticEquation", () => {
+  it("is a function", () => {
+    expect(quadraticEquation).toBeInstanceOf(Function);
+  });
+  [
+    [1, -2, -3, [3, -1]],
+    [-1, -2, 15, [-5, 3]],
+    [1, 12, 36, [-6, -6]],
+    [6, 2, 1, ["No x"]],
+  ].forEach(([a, b, c, result]) => {
+    it(`solves quadratic equation ${a}x^2+${b}x+${c}=0 with x 
+    equal to ${result}`, () => {
+      expect(quadraticEquation(a, b, c)).toStrictEqual(result);
+    });
+  });
+});
