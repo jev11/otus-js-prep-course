30474d042b13791feb90de3d99aeac0c9a36a5f1
Barannikov, Evgeny (VodafoneZiggo) <evgeny.barannikov@vodafoneziggo.com>
2021-05-13 18:24:43 +0200

    added test for input in assignment 7

--- src/7/script.test.js
+++ src/7/script.test.js
@@ -19,22 +19,30 @@ describe("createList", () => {
     expect(el.querySelector("button").hidden).toBeTruthy();
   });
 
+  it("makes button visible after registering input event", () => {
+    el.querySelector("input").value = "abcd";
+    el.querySelector("input").dispatchEvent(new Event("input"));
+    expect(el.querySelector("button").hidden).toBeFalsy();
+  });
+
   it("clears input on form after click", () => {
     el.querySelector("input").value = "abcd";
-    el.querySelector("button").click();
+    expect(el.querySelector("input").value).toBe("abcd");
+    el.querySelector("button").dispatchEvent(new Event("click"));
     expect(el.querySelector("input").value).toBe("");
+    expect(el.querySelector("button").hidden).toBeTruthy();
   });
 
   it("renders paragraph in history", () => {
     el.querySelector("input").value = "efgh";
-    el.querySelector("button").click();
+    el.querySelector("button").dispatchEvent(new Event("click"));
     expect(el.querySelector(".history").innerHTML).toBe("<p>efgh</p>");
   });
 
   it("keeps only last 5 entries in history", () => {
     ["1", "2", "3", "4", "5", "6"].forEach((p) => {
       el.querySelector("input").value = p;
-      el.querySelector("button").click();
+      el.querySelector("button").dispatchEvent(new Event("click"));
     });
     expect(el.querySelector(".history").innerHTML).toBe(
       "<p>6</p><p>5</p><p>4</p><p>3</p><p>2</p>"
